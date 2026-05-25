3100d981d24ad342c0a1bb66c4202328f91dfa29
이지민 <130419090+vahkjsdf@users.noreply.github.com>
2026-05-25 23:50:56 +0900

    [week3] 모음사전

---
+++ jimin-lee/lv2_모음사전_jimin-lee.kt
@@ -0,0 +1,30 @@
+class Solution {
+    fun solution(word: String): Int {
+
+        var answer = 0
+        var count = 0
+
+        val vowels = listOf('A', 'E', 'I', 'O', 'U')
+
+        fun dictionary(char: String) {
+
+            if (char.length > 5) return
+
+            if (char != "") {
+                count++
+            }
+
+            if (char == word) {
+                answer = count
+            }
+
+            for (vowel in vowels) {
+                dictionary(char + vowel)
+            }
+        }
+
+        dictionary("")
+
+        return answer
+    }
+}
